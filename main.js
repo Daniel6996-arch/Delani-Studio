f5b12856183ea3a56a2440c0a14cb327807cc2a9
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-10 21:23:54 +0300

    add backgroung-image to contact form

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-       var imageUrl = "assets/backgrounds/first.jpg"
-       var imageUrlOne = "assets/backgrounds/second.jpg"
+       var imageUrl = "assets/backgrounds/first.jpg";
+       var imageUrlOne = "assets/backgrounds/second.jpg";
+       var imageUrlTwo = "assets/backgrounds/third.jpg";
         $(".bg-one").css("background-image","url("+ imageUrl +")");
-        $(".bg-one").css("background-position","center")
+        $(".bg-one").css("background-position","center");
         $(".bg-two").css("background-image","url("+ imageUrlOne +")");
-        $(".bg-two").css("background-position","center")
-        $(".bg-two").css("background-repeat","no-repeat")
+        $(".bg-two").css("background-position","center");
+        $(".bg-two").css("background-repeat","no-repeat");
+        $(".bg-three").css("background-image","url("+ imageUrlTwo +")");
+        $(".bg-three").css("background-position","center");
+        $(".bg-three").css("background-repeat","no-repeat");
 
     });
