c58df9bb76290b7fb06c211a62a6cbfab00ef814
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-11 18:11:39 +0300

    add more fuctionality to form

--- main.js
+++ main.js
@@ -42,8 +42,12 @@ $(document).ready(function(){
                 var email = $("#email").val();
                 var text = $("#textarea").val();
                 var output = [];
-                var output = output.concat(name, email, text);
-                console.log(output)
+                var outputs = output.concat(name, email, text);
+                //console.log(output)
+                var sent = output.map(function(output){
+                       return output += 1;
+                });
+                console.log(sent)
         });
 });
 
