569be12af0d6e26bb532fc6f11ba7b37c1c845f1
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-11 09:26:37 +0300

    add cursor as pointer for desginated places

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+var imageUrl = "assets/backgrounds/first.jpg";
+var imageUrlOne = "assets/backgrounds/second.jpg";
+var imageUrlTwo = "assets/backgrounds/third.jpg";
+
 $(document).ready(function(){
-       var imageUrl = "assets/backgrounds/first.jpg";
-       var imageUrlOne = "assets/backgrounds/second.jpg";
-       var imageUrlTwo = "assets/backgrounds/third.jpg";
         $(".bg-one").css("background-image","url("+ imageUrl +")");
         $(".bg-one").css("background-position","center");
         $(".bg-two").css("background-image","url("+ imageUrlOne +")");
@@ -11,5 +12,6 @@ $(document).ready(function(){
         $(".bg-three").css("background-position","center");
         $(".bg-three").css("background-repeat","no-repeat");
         $(".soci").css("display", "none");
+        $()
+});
 
-    });
